d8771c3dc653cd3bd9d4e5121d366189d68662b6
Dinuka Kasun <dinuka.kasunds@gmail.com>
2024-05-01 22:30:47 +0530

    drawing-paths-and-shapes

---
+++ Landmarks/View/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,23 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Dinuka Kasun on 2024-05-01.
+//
+
+import SwiftUI
+
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
