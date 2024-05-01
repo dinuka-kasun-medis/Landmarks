8dfaa6f7b68b59a422b05190b88b8392909b5e0b
Dinuka Kasun <dinuka.kasunds@gmail.com>
2024-05-01 20:40:30 +0530

    handling-user-input

---
+++ Landmarks/View/Helper/FavoriteButton.swift
@@ -0,0 +1,25 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by Dinuka Kasun on 2024-05-01.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundStyle(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+#Preview {
+    FavoriteButton(isSet: .constant(true))
+}
